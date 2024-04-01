2374fd8541347e5ee19ccf993cef6ae06867aa7d
Noe <noe.flores@tecsup.edu.pe>
2024-04-01 11:46:12 -0500

    version2

--- app.js
+++ app.js
@@ -2,32 +2,35 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Datos de ejemplo
+// Datos de ejemplo actualizados
 const clientes = [
-  { id: 1, nombre: 'Cliente 1' },
-  { id: 2, nombre: 'Cliente 2' },
-  { id: 3, nombre: 'Cliente 3' }
+    { id: 1, nombre: 'Juan ' },
+    { id: 2, nombre: 'Mario ' },
+    { id: 3, nombre: 'pedro' }
+];
+const productos = [
+    { id: 1, nombre: 'iPhone' },
+    { id: 2, nombre: 'MacBook Pro' },
+    { id: 3, nombre: 'Echo Dot' }
 ];
 
-const productos = [
-  { id: 1, nombre: 'Producto 1' },
-  { id: 2, nombre: 'Producto 2' },
-  { id: 3, nombre: 'Producto 3' }
-];
+
+// Configuración del motor de plantillas EJS
+app.set('view engine', 'ejs');
 
 // Rutas
 app.get('/', (req, res) => {
-  res.send('Bienvenido a mi aplicación Express, lab 04');
+    res.render('index');
 });
 
 app.get('/clientes', (req, res) => {
-  res.json(clientes);
+    res.render('clientes', { clientes });
 });
 
 app.get('/productos', (req, res) => {
-  res.json(productos);
+    res.render('productos', { productos });
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
