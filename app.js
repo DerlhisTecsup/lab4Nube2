6cb1bfaea2837f6832428ef14ea50f83ed5f1e81
Johan Cruz <johan.cruz@tecsup.edu.pe>
2024-04-11 22:26:42 -0500

    repositotio curso nube

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Datos de ejemplo actualizados
 const clientes = [
-    { id: 1, nombre: 'Juan ' },
-    { id: 2, nombre: 'Mario ' },
-    { id: 3, nombre: 'pedro' }
+  { id: 1, nombre: "Johan " },
+  { id: 2, nombre: "Derlhis " },
+  { id: 3, nombre: "Franclin" },
 ];
 const productos = [
-    { id: 1, nombre: 'iPhone' },
-    { id: 2, nombre: 'MacBook Pro' },
-    { id: 3, nombre: 'Echo Dot' }
+  { id: 1, nombre: "cocina" },
+  { id: 2, nombre: "television" },
+  { id: 3, nombre: "celular" },
 ];
 
-
 // Configuración del motor de plantillas EJS
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Rutas
-app.get('/', (req, res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/clientes', (req, res) => {
-    res.render('clientes', { clientes });
+app.get("/clientes", (req, res) => {
+  res.render("clientes", { clientes });
 });
 
-app.get('/productos', (req, res) => {
-    res.render('productos', { productos });
+app.get("/productos", (req, res) => {
+  res.render("productos", { productos });
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
